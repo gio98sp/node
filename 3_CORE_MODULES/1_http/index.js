2dffade53c4ccd4d3e58ca484558eb4bde4f09dd
Giovane Santos <giovane98sp@gmail.com>
2023-07-03 15:50:15 -0300

    add lesson 3.1 http

---
+++ 3_CORE_MODULES/1_http/index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.write('Oi Http')
+  res.end()
+})
+
+server.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`)
+  console.log(`acesse: http://localhost:${port}`)
+})

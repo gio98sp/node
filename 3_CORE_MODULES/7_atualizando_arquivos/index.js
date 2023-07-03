4f7c0364480e67f2d19106e02774481d6d431487
Giovane Santos <giovane98sp@gmail.com>
2023-07-03 19:43:29 -0300

    add lesson 3.7

---
+++ 3_CORE_MODULES/7_atualizando_arquivos/index.js
@@ -0,0 +1,29 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  const urlInfo = url.parse(req.url, true)
+  const name = urlInfo.query.name
+
+  if (name) {
+    const nameNewLine = name + ',\r\n'
+    fs.appendFile('arquivo.txt', nameNewLine, (err) => {
+      res.writeHead(302, { Location: '/' })
+      res.end()
+    })
+  } else {
+    fs.readFile('index.html', (err, data) => {
+      res.writeHead(200, { 'Content-Type': 'text/html'})
+      res.write(data)
+      res.end()
+    })
+  }
+})
+
+server.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+  console.log(`Acesse: http://localhost:${port}`)
+})

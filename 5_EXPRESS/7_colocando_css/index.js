709dcfa0419b57aa16b56bc476267c1e8e15fda5
Giovane Santos <giovane98sp@gmail.com>
2023-07-06 19:42:19 -0300

    add lesson 5.7

---
+++ 5_EXPRESS/7_colocando_css/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+const basePath = path.join(__dirname, 'templates')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}`)
+})

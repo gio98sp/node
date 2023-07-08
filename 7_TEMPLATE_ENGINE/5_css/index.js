98314ab8a17dd213b8d1ec370f236505d44f6618
Giovane Santos <giovane98sp@gmail.com>
2023-07-08 15:53:58 -0300

    add lesson 7.5

---
+++ 7_TEMPLATE_ENGINE/5_css/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(3000)
